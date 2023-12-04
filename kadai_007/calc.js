7acd93920b6c396b23dbf6482f2504a0845c760b
hama1117 <87461496+hama1117@users.noreply.github.com>
2023-12-05 07:23:36 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,30 @@
+// 変数numに1以上のランダムな整数を代入する
+let num = Math.floor(Math.random() *15)+1;
+
+// 変数numの値を出力する
+console.log(num);
+
+// 変数numの値によって出力する文字列を切り替える
+switch (num) {
+   case 3:
+    console.log('3の倍数です');
+    break;
+   case 6:
+    console.log('3の倍数です');
+    break;
+   case 9:
+    console.log('3の倍数です');
+    break;
+   case 12:
+    console.log('3の倍数です');
+    break;
+   case 15:
+    console.log('3と5の倍数です');
+    break;
+   case 5:
+    console.log('5の倍数です');
+    break;
+   case 10:
+    console.log('5の倍数です');
+    break;
+}
