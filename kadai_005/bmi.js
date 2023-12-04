2c62a25020c0aae04cb194348a4c211c3c748023
hama1117 <87461496+hama1117@users.noreply.github.com>
2023-12-04 13:21:42 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,15 @@
+let userWeight;
+
+userWeight = '体重'
+
+let userHeight;
+
+userHeight = '身長'
+
 let userBmi;
 
 userBmi = 68 / (1.7 * 1.7);
 
+console.log(userWeight);
+console.log(userHeight);
 console.log(userBmi);
