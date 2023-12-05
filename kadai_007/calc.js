f942fc3dafa8fc80324c1f123dafbc73e2ebe199
hama1117 <87461496+hama1117@users.noreply.github.com>
2023-12-05 21:29:06 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,28 +1,18 @@
 // 変数numに1以上のランダムな整数を代入する
-let num = Math.floor(Math.random() *15)+1;
+let num = Math.floor(Math.random() * 100)+1;
 
 // 変数numの値を出力する
 console.log(num);
 
 // 変数numの値によって出力する文字列を切り替える
-if (num === 3) {
-    console.log('3の倍数です');
-}
-if (num === 6) {
-    console.log('3の倍数です');
-}
-if (num === 9) {
-    console.log('3の倍数です');
-}
-if (num === 12) {
-    console.log('3の倍数です');
-}
-if (num === 5) {
-    console.log('5の倍数です');
-}
-if (num === 10) {
-    console.log('5の倍数です');
-}
-if (num === 15) {
+if (num % 3 === 0 && num % 5 === 0) {
     console.log('3と5の倍数です');
+}
+
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+
+else if (num % 5 === 0) {
+    console.log('5の倍数です');
 }
