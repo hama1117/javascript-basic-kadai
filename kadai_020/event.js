6fdd871a9fdeb6dbc47d481cd050c90013eb0f48
hama1117 <87461496+hama1117@users.noreply.github.com>
2023-12-15 16:11:25 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+  const addBtn = document.getElementById('btn');
+  const addText = document.getElementById('text');
+
+  addBtn.addEventListener('click', () => {
+    addText.innerHTML = 'ボタンをクリックしました'
+  });
